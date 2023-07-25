07e5607f316472924c1e28e4a5c5554a828d33c0
M.N. Jeral Sandeeptha <jeral.sandeeptha1@gmail.com>
2023-07-25 21:53:17 +0530

    Fetch all posts

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -1,26 +1,50 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom';
 import '../styles/Post.css';
+import sanityClient from '../client';
 
 const Post = () => {
+
+  const [posts, setPosts] = useState([]);
+
+  useEffect( () => {
+    sanityClient.fetch(`*[_type == "post"]{
+      title,
+      slug,
+      mainImage{
+        asset->{
+          _id,
+          url
+        }
+      },
+      author,
+      body
+    }`).then( (data) => {
+      setPosts(data);
+      console.log(data);
+    }).catch( error => console.log(error));
+  }, []);
+
   return (
     <div className='blog'>
       <div className="blog-inner">
         <h1 className='header'>BLOG POSTS PAGE</h1>
         <h5 className="sub-header">Welcome to my blog page</h5>
         <div className="posts">
-            <div className="post">
-              <img src="https://img.freepik.com/free-photo/handsome-successful-businessman-full-suit-opens-his-car-streets-city-near-modern-office-center_496169-609.jpg" alt="" />
-              <h3 className='title'>Title</h3>
-            </div>
-            <div className="post">
-              <img src="https://img.freepik.com/free-photo/handsome-successful-businessman-full-suit-opens-his-car-streets-city-near-modern-office-center_496169-609.jpg" alt="" />
-              <h3 className='title'>Title</h3>
-            </div>
-            <div className="post">
-              <img src="https://img.freepik.com/free-photo/handsome-successful-businessman-full-suit-opens-his-car-streets-city-near-modern-office-center_496169-609.jpg" alt="" />
-              <h3 className='title'>Title</h3>
-            </div>
+        {posts.length === 0 ? (
+          <p>No posts available</p>
+        ) : (
+          posts.map((post) => {
+            return (
+              <Link to={`/post/${post.slug.current}`} key={post.slug.current}>
+                <div className="post">
+                  <img src={post.mainImage.asset.url} alt="image" />
+                  <h3 className="title">{post.title}</h3>
+                </div>
+              </Link>
+            );
+          })
+        )}
         </div>
       </div>
     </div>
